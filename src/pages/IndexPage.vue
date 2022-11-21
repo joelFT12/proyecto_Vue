c7d02106cd8c3672c52dc9c302c566abbc71fa44
Figueroa <alex.figue503@gmail.com>
2022-11-20 20:34:59 -0600

    sideBar

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page style="background-color: #FBFAFA;" class="">
+  <q-page style="background-color: #F2F2F2;" class="">
     <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
@@ -7,61 +7,90 @@
     > -->
 
     <div class="row">
-      <div style="min-height: 100vh; max-width: 250px;" class="col bg-blue-1 gt-sm hidde q-mr-sm">
+      <q-scroll-area style="height: 100vh; max-width: 250px;"
+        class="col bg-white q-pt-lg q-pl-sm q-pr-sm  gt-sm hidde q-mr-sm">
 
-        <q-list bordered separator>
-          <q-item clickable v-ripple>
+        <q-list bordered separator class="text-center">
+          <span class="text-h6 text-primary">Marcas</span>
+          <q-item clickable v-ripple class="">
             <q-item-section>
-              <q-checkbox v-model="right" label="Label on Right" />
+              <q-checkbox v-model="right" label="Samsung" />
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple class="">
             <q-item-section>
-              <q-checkbox v-model="right" label="Label on Right" />
+              <q-checkbox v-model="right" label="Huawei" />
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple class="">
             <q-item-section>
-              <q-checkbox v-model="right" label="Label on Right" />
+              <q-checkbox v-model="right" label="Nokia" />
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple class="">
             <q-item-section>
-              <q-checkbox v-model="right" label="Label on Right" />
+              <q-checkbox v-model="right" label="Xiaomi" />
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple>
+        </q-list>
+        <q-list bordered separator class="q-mt-lg text-center">
+          <span class="text-h6 text-primary">Sistema</span>
+          <q-item clickable v-ripple class="">
             <q-item-section>
-              <q-checkbox v-model="right" label="Label on Right" />
+              <q-checkbox v-model="right" label="Android" />
             </q-item-section>
           </q-item>
-          <q-item clickable v-ripple>
+          <q-item clickable v-ripple class="">
             <q-item-section>
-              <q-checkbox v-model="right" label="Label on Right" />
+              <q-checkbox v-model="right" label="Windows" />
+            </q-item-section>
+          </q-item>
+          <q-item clickable v-ripple class="">
+            <q-item-section>
+              <q-checkbox v-model="right" label="ios" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+        <q-list bordered separator class="q-mt-lg text-center">
+          <span class="text-h6 text-primary">Pantalla</span>
+          <q-item clickable v-ripple class="">
+            <q-item-section>
+              <q-checkbox v-model="right" label="Android" />
+            </q-item-section>
+          </q-item>
+          <q-item clickable v-ripple class="">
+            <q-item-section>
+              <q-checkbox v-model="right" label="Windows" />
+            </q-item-section>
+          </q-item>
+          <q-item clickable v-ripple class="">
+            <q-item-section>
+              <q-checkbox v-model="right" label="ios" />
             </q-item-section>
           </q-item>
         </q-list>
 
-      </div>
+      </q-scroll-area>
+      <!-- Here -->
       <div class="col">
         <div class="row q-pr-xs q-mt-xs">
           <div class="col-lg-3 col-md-4 col-xs-6 q-pa-xs q-mt-md" v-for="(item, key) in 5" :key="key">
             <q-card class=" my-card" flat bordered>
-            <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <img src="https://cdn.quasar.dev/img/parallax2.jpg">
 
-            <q-card-section>
-              <div class="text-h6">$132.00</div>
-            </q-card-section>
+              <q-card-section>
+                <div class="text-h6">$132.00</div>
+              </q-card-section>
 
-            <q-card-section class="q-pt-none text-justify">
-              Samsung J6, Pantalla de 5.5
+              <q-card-section class="q-pt-none text-justify">
+                Samsung J6, Pantalla de 5.5
                 64GB, 2GB Ram, Color Negro
-            </q-card-section>
-            <q-separator />
-            <q-card-actions>
-              <q-btn flat color="primary">Action 2</q-btn>
-            </q-card-actions>
-          </q-card>
+              </q-card-section>
+              <q-separator />
+              <q-card-actions>
+                <q-btn flat color="primary">Action 2</q-btn>
+              </q-card-actions>
+            </q-card>
           </div>
         </div>
       </div>
