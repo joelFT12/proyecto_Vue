cab093e0bb1ff17e89c09884e47e40ea87d9123d
Figueroa <alex.figue503@gmail.com>
2022-11-21 23:47:36 -0600

    sort date

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -21,13 +21,13 @@
       <div class="col ">
         <div class="row q-mt-md flex  justify-center items-center ">
           <div class="col-lg-2 q-mr-lg lt-md hidde">
-            <q-select outlined  v-model="sortBy" :options="sortOption" color="primary"/>
+            <q-select outlined v-model="sortBy" :options="sortOption" color="primary" @update:model-value="sortSelect"/>
           </div>
           <div class="col-lg-5 gt-sm hidde">
             <div class=" flex justify-center items-center">
               <label for="" class="q-pr-md">ordenar por:</label>
               <q-btn-toggle v-model="sortBy" class="my-custom-toggle" no-caps rounded unelevated toggle-color="primary"
-                color="white" text-color="primary" :options="sortOption" />
+                color="white" text-color="primary" :options="sortOption" @click="sortCards" />
             </div>
           </div>
         </div>
@@ -58,13 +58,13 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 const articulosOriginales = [
-  { id: '001', precio: 24.25, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2022-10-05' },
-  { id: '002', precio: 60.05, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2022-10-05' },
-  { id: '003', precio: 46.32, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2022-10-05' },
-  { id: '004', precio: 10.89, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2022-10-05' },
-  { id: '005', precio: 67.66, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2022-10-05' },
-  { id: '006', precio: 67.25, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2022-10-05' },
-  { id: '007', precio: 36.02, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2022-10-05' }
+  { id: '001', precio: 24.25, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2013-06-03' },
+  { id: '002', precio: 60.05, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2013-01-03' },
+  { id: '003', precio: 46.32, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2013-01-02' },
+  { id: '004', precio: 10.89, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2013-01-04' },
+  { id: '005', precio: 67.66, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2013-10-03' },
+  { id: '006', precio: 67.25, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2013-07-30' },
+  { id: '007', precio: 36.02, titulo: 'Samsung J6, Pantalla de 5.5 64GB, 2GB Ram, Color Negro', vendedor: 'juan perez', telefono: '806045-3456', fecha: '2013-08-30' }
 ]
 
 const articulos = ref([])
@@ -73,10 +73,36 @@ const sortOption = ref([
   { label: 'PRECIO', value: 'PRECIO' },
   { label: 'FECHA', value: 'FECHA' }
 ])
+
+function sortCards () {
+  console.log(sortBy.value)
+  if (sortBy.value === 'PRECIO') {
+    articulos.value.sort((a, b) => a.precio - b.precio)
+  }
+  if (sortBy.value === 'FECHA') {
+    articulos.value.sort((a, b) => {
+      if (a.fecha < b.fecha) {
+        return -1
+      }
+      if (a.fecha > b.fecha) {
+        return 1
+      }
+
+      return 0
+    })
+  }
+}
+
+function sortSelect (value) {
+  sortBy.value = value.value
+  sortCards()
+}
+
 onMounted(() => {
   articulosOriginales.forEach(item => {
     articulos.value.push(item)
   })
+  sortCards()
 })
 
 </script>
